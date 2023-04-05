078f92fb27393d481d3a842547cf525eaa3cb4fe
HeavenIam <cielocalderon02@gmail.com>
2023-04-04 20:53:06 -0500

    agregando funcionalidad de formulario

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
+
+// obtener el formulario
+const formulario_login = document.getElementById('login');
+
 // obtener el input
 const input_usuario = document.getElementById('Usuario');
 
-// Obtener boton
-const boton_entrar = document.getElementById('Entrar');
-
-const InputValue = input_usuario.value;
 
 
-// escuchar evento de click
-boton_entrar.addEventListener('click', ()=> {
-    alert(`Hola ${InputValue}`)
-})
+// comenzar variable de ultimo valor del input
+let ultimoValor = '';
+
+
+
+// Agregar un event listener al input para que escuche cuando el usuario escribe
+input_usuario.addEventListener('input', (event) => {
+    ultimoValor = event.target.value;
+});
+
+
+
+// Agregar un event listener al formulario para escuchar el evento submit
+
+formulario_login.addEventListener('submit', enviarFormulario)
+
+function enviarFormulario(event) {
+
+    event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario de recargar la pagina
+
+    const nombreUsuario = ultimoValor;
+    alert(`Hola ${nombreUsuario}`)
+}
+
+
+
 
 
 
